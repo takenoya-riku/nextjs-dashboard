56d11fac58d44961ecba442959533b340d5e60ec
takenoya <takenoya@e3sys.co.jp>
2025-09-05 09:32:21 +0900

    dashboardページのロールバック

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,15 +8,15 @@ import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardSkeleton } from "@/ap
 export default async function Page() {
     return (
         <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+            <h1 className={`${lusitana.className} mb-4 text-xl`}>
                 Dashboard
             </h1>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <div>
                 <Suspense fallback={<CardSkeleton />}>
                     <CardWrapper />
                 </Suspense>
             </div>
-            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+            <div>
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
                 </Suspense>
