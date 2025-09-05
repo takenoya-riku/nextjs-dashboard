ba77bfa06a40db152bb7f04b51e344765d399476
takenoya <takenoya@e3sys.co.jp>
2025-09-05 10:02:40 +0900

    請求リストコンポーネントのロールバック

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -13,16 +13,13 @@ export default async function LatestInvoices() {
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         NOTE: Uncomment this code in Chapter 7
 
-        <div className="bg-white px-6">
+        <div className="px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
-                  {
-                    'border-t': i !== 0,
-                  },
+                  'flex flex-row items-center justify-between py-4'
                 )}
               >
                 <div className="flex items-center">
@@ -37,7 +34,7 @@ export default async function LatestInvoices() {
                     <p className="truncate text-sm font-semibold md:text-base">
                       {invoice.name}
                     </p>
-                    <p className="hidden text-sm text-gray-500 sm:block">
+                    <p className="text-sm text-gray-500">
                       {invoice.email}
                     </p>
                   </div>
