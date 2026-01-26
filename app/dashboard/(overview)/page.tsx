13055a1749bd7f7e9c049470f8296688ab7ae84a
takenoya <takenoya@e3sys.co.jp>
2026-01-26 11:43:12 +0900

    A-1初期値修正、再設定

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -21,7 +21,7 @@ export default async function Page() {
                     <RevenueChart />
                 </Suspense>
                 <Suspense fallback={<LatestInvoicesSkeleton />}>
-                <LatestInvoices />
+                    <LatestInvoices />
                 </Suspense>
             </div>
         </main>
