71a7364edee2ce73dd6fa4f602aa9d5db731433c
takenoya <takenoya@e3sys.co.jp>
2026-01-26 11:47:13 +0900

    A-1実装済み

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,12 +11,12 @@ export default async function Page() {
             <h1 className={`${lusitana.className} mb-4 text-xl`}>
                 Dashboard
             </h1>
-            <div>
+            <div className="grid grid-cols-4 gap-4">
                 <Suspense fallback={<CardSkeleton />}>
                     <CardWrapper />
                 </Suspense>
             </div>
-            <div>
+            <div className="grid grid-cols-2 gap-4 mt-6">
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
                 </Suspense>
